433df3e9ebda2d70b5f28072be90fb3b73275fb3
emrahelis <emrahelis@gmail.com>
2024-09-29 10:01:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+// Name Space nedir : Programlamada, başlangıç aşamasında extra hiç bir kütüphane kullanmamıza ihtiyaç yoktur.
+
+// ancak ilerleyen yerlede bazı kütüphanelere ihtiyaç duyarız. Bu kütüphaneler, dışarıdan indirilen
+// kütüphaneler olabileceği gibi, .net'in kendi içerisinde olan kütüphaneler olabilir.
+// Namespace aslında, aynı isimde olan tipleri birbirinden ayırmak ve içinde tutmak için varıdr.
+
+// bir namespace çalırmak için using ifadesi kullanılır.
+// 
+
+// 
+System.Console.WriteLine();
+
+Queue u = new Queue();
